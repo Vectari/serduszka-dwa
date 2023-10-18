3a0d7bcd6b9097422eec25457811eec1efdfb5e7
Mateusz Majer <mateusz.majer996@gmail.com>
2023-10-18 23:02:09 +0200

    add header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import logo from "../../theme/logo.png";
+
+const StyledTitle = styled.p`
+  margin-top: 1rem;
+  margin-bottom: 0.6rem;
+  font-family: "Great Vibes";
+  font-size: 2.1rem;
+`;
+
+const StyledImg = styled.img`
+  width: 5rem;
+  display: inline-block;
+  vertical-align: middle;
+  margin-top: -1.3rem;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <StyledTitle>
+        Serduszka Dwa <StyledImg src={logo} alt="logo" />
+      </StyledTitle>
+    </>
+  );
+};
+
+export default Header;
