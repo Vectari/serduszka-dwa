55ede7c55a01e8d51473ff8327672e5c4f113a4c
Vectari <mateusz.majer996@gmail.com>
2024-07-24 22:13:54 +0200

    feat: add new component to page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Rec from "./components/recommendations/recommendations";
 import PriceList from "./components/pricelist/pricelist";
 import Questions from "./components/questions/questions";
 import Contact from "./components/contact/contact";
+import { TopPageInfo } from "./components/toppageinfo/TopPageInfo";
 
 function App() {
   return (
     <>
       <NavBar />
       <Header />
+      <TopPageInfo info={"Z powodu przerwy wakacyjnej biuro nieczynne od 20.07. do 30.07."} />
       <Main />
       <Rec />
       <PriceList />
