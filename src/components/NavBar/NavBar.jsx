e9528cb1acb495ef29ce831a1c06c665f093cd14
Vectari <mateusz.majer996@gmail.com>
2025-02-22 21:13:53 +0100

    feat: add NavLinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export function NavBar() {
-  return <h1>NavBar</h1>;
+  return (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/info">Info</NavLink>
+      <NavLink to="/pricelist">Price List</NavLink>
+      <NavLink to="/reviews">Reviews</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/map">Map</NavLink>
+      <NavLink to="/alert">Alert</NavLink>
+    </>
+  );
 }
