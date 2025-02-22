26a5021c70d85e440d905df0306537d24eeb3fd2
Vectari <mateusz.majer996@gmail.com>
2025-02-22 20:53:11 +0100

    feat: add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,42 @@
 import { createRoot } from "react-dom/client";
 import { StyledBody } from "./main.styled";
-import { Button } from "./components/Button/Button";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Home } from "./view/Home/Home";
+import { Contact } from "./view/Contact/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <>
+            <Home />
+          </>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        // path: "/signup",
+        // element: <SignUp />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StyledBody>
-    <p>test</p>
-    <p>test branch deploy</p>
-    <Button />
+    <RouterProvider router={router} />
   </StyledBody>
 );
