04b6dae8a45c6c57cc21be28f0d207283821a6d9
Vectari <mateusz.majer996@gmail.com>
2025-03-18 17:13:47 +0100

    feat: color palete update

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+  background: "#ffffff",
+  text_on_background: "#545454",
+
+  navbar_text: "#545454",
+  navbar_text_active: "#c6b790",
+
+  button_primary: "#c6b790",
+  button_primary_text: "#545454",
+
+  section_one_background: "#a09e77",
+  section_one_text_on_background: "#f5f4f0",
+  section_one_btn_background: "#f5f4f0",
+  section_one_btn_text: "#545454",
+
+  section_two_background: "#f5f4f0",
+  section_two_text_on_background: "#545454",
+  section_two_btn_background: "#646464",
+  section_two_btn_text: "#545454",
+
+  alert_green: "#a4bf7c",
+  alert_red: "#c0857f",
+  alert_text: "#545454",
+
+  border_and_lines: "#e8e8e8",
+};
