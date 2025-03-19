4fdb9e0a009b3d480dca797e12d595f7e8866111
Vectari <mateusz.majer996@gmail.com>
2025-03-19 23:35:55 +0100

    style: added style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,23 @@
-export function Button({ title }) {
-  return <button>{title}</button>;
+/* eslint-disable react/prop-types */
+
+import styled from "styled-components";
+import { theme } from "../../theme";
+import { NavLink } from "react-router-dom";
+
+const StyledNavLink = styled(NavLink)`
+  display: inline-block;
+  text-decoration: none;
+  color: ${theme.button_primary_white};
+  background-color: ${theme.button_primary_background};
+  border: none;
+  border-radius: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 1.3rem;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export function Button({ title, to }) {
+  return <StyledNavLink to={to}>{title}</StyledNavLink>;
 }
