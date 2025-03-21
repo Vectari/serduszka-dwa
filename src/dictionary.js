65a0d6bd57210e1de91a266822764f9f61566e9c
Vectari <mateusz.majer996@gmail.com>
2025-03-21 22:33:15 +0100

    feat: content update

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,4 +1,16 @@
 export const dictionary = {
+  main_info: {
+    phone_numer: "123123123",
+    email: "biuroserduszkadwa@gmail.com",
+  },
+  navbar: {
+    home_page: "Główna",
+    info_page: "Informacje",
+    price_page: "Cennik",
+    reviews_page: "Opinie Klientów",
+    contact_page: "Kontakt",
+    map_page: "Dojazd",
+  },
   home_page: {
     name: "Serduszka Dwa",
     description: "Biuro Matrymonialne i Partnerskie",
