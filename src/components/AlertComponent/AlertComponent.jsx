5370818e6ca8db07ea814742db7d0167b49a046c
Vectari <mateusz.majer996@gmail.com>
2025-03-28 22:51:45 +0100

    feat: added basic content

--- src/components/AlertComponent/AlertComponent.jsx
+++ src/components/AlertComponent/AlertComponent.jsx
@@ -1,7 +1,17 @@
+import styled from "styled-components";
+
+const StyledAlertComponent = styled.div`
+  color: black;
+  background-color: #f72c2c;
+  text-align: center;
+  margin: -1rem;
+  border-radius: 1rem;
+`;
+
 export function AlertComponent() {
   return (
-    <>
-      <h1>Alert Component</h1>
-    </>
+    <StyledAlertComponent>
+      <h3>23.05 - 30.05 - Lorem ipsum dolor sit amet.</h3>
+    </StyledAlertComponent>
   );
 }
