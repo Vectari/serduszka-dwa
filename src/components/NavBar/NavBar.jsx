f5b3712770d8483fe974b20842d22c2ad8e47310
Vectari <mateusz.majer996@gmail.com>
2025-03-29 12:32:38 +0100

    feat: strings from dictionary

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { theme } from "../../theme";
 import { Logo } from "../Logo/Logo";
+import { dictionary } from "../../dictionary";
 
 const StyledNavBar = styled.div`
   display: flex;
@@ -54,18 +55,18 @@ export function NavBar() {
   return (
     <StyledNavBar>
       <StyledLeftContainer>
-        <NavLink to="/">Główna</NavLink>
-        <NavLink to="/info">Informacje</NavLink>
-        <NavLink to="/pricelist">Cennik</NavLink>
+        <NavLink to="/">{dictionary.navbar.home_page}</NavLink>
+        <NavLink to="/info">{dictionary.navbar.info_page}</NavLink>
+        <NavLink to="/pricelist">{dictionary.navbar.price_page}</NavLink>
       </StyledLeftContainer>
       <StyledLogoContainer>
         <Logo />
         <p>Serduszka Dwa</p>
       </StyledLogoContainer>
       <StyledRightContainer>
-        <NavLink to="/reviews">Opinie klientów</NavLink>
-        <NavLink to="/contact">Kontakt</NavLink>
-        <NavLink to="/map">Dojazd</NavLink>
+        <NavLink to="/reviews">{dictionary.navbar.reviews_page}</NavLink>
+        <NavLink to="/contact">{dictionary.navbar.contact_page}</NavLink>
+        <NavLink to="/map">{dictionary.navbar.map_page}</NavLink>
       </StyledRightContainer>
       {/* <NavLink to="/alert">Alert</NavLink> */}
     </StyledNavBar>
