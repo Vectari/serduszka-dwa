15d8e82d1eae53047bd417cd681e9ade8772fb94
Vectari <mateusz.majer996@gmail.com>
2025-03-31 23:30:16 +0200

    chore: cleanup in media

--- src/view/PriceList/PriceList.jsx
+++ src/view/PriceList/PriceList.jsx
@@ -1,3 +1,14 @@
 export function PriceList() {
-  return <h1>Proce List</h1>;
+  return (
+    <h1>
+      Cennik Opłaty za korzystanie z usług biura "Serduszka Dwa" 📌 Abonament na
+      6 miesięcy – 700 zł 📌 Jednorazowa oferta (1 propozycja poznania
+      kandydata) – 260-300 zł 📌 Oferta VIP (specjalne wymagania, zwiększona
+      trudność w doborze kandydatów) – cena ustalana indywidualnie Istnieje
+      możliwość płatności w dwóch ratach. Co obejmuje opłata? ✔ Profesjonalne
+      dobranie kandydatów na podstawie Twojego profilu i oczekiwań ✔
+      Przedstawienie ofert i zdjęć kandydatów ✔ Wymiana kontaktów po obustronnej
+      zgodzie ✔ Możliwość skorzystania z ofert na wspólne wyjścia i wyjazdy
+    </h1>
+  );
 }
