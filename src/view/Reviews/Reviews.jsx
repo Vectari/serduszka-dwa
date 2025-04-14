66fef56b8c7e8a88a77697bfcecabc214c8b1120
Vectari <mateusz.majer996@gmail.com>
2025-04-14 22:23:37 +0200

    feat: add google reviews img

--- src/view/Reviews/Reviews.jsx
+++ src/view/Reviews/Reviews.jsx
@@ -1,3 +1,33 @@
+import styled from "styled-components";
+import GOOGLE_REVIEW from "../../media/google_reviews.png";
+import { NavLink } from "react-router-dom";
+import { theme } from "../../theme";
+
+const ReviewWrapper = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  text-decoration: none;
+  color: ${theme.navbar_text};
+  text-align: center;
+`;
+
+const StyledImg = styled.img`
+  width: 55%;
+  height: 60%;
+  border-radius: 1rem;
+  object-fit: cover;
+  object-position: center;
+  margin: auto;
+`;
+
 export function Reviews() {
-  return <h1>Reviews</h1>;
+  return (
+    <>
+      <ReviewWrapper to="https://search.google.com/local/writereview?placeid=ChIJNRzA9d0TA0cRZ2J7UHzeIHY">
+        <StyledImg src={GOOGLE_REVIEW} alt="google reviews" />
+        <h3>Oceń nas w Google!</h3>
+      </ReviewWrapper>
+      <h2>listy</h2>
+    </>
+  );
 }
