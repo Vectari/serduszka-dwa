6e4f5f197e4f12f03102461fe8e998383c73b480
Vectari <mateusz.majer996@gmail.com>
2025-04-17 21:33:23 +0200

    style: add styles

--- src/view/PriceList/PriceList.jsx
+++ src/view/PriceList/PriceList.jsx
@@ -1,20 +1,58 @@
+import styled from "styled-components";
 import { dictionary } from "../../dictionary";
+import { theme } from "../../theme";
+
+const PriceListWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledPriceHeader = styled.h2`
+  color: ${theme.section_two_text_on_background};
+  background-color: ${theme.section_two_background};
+  border-radius: 1rem;
+  margin: auto;
+  padding: 0.5rem 1rem;
+  max-width: 90%;
+`;
+
+const StyledPrice = styled.p`
+  color: ${theme.section_one_text_on_background};
+  background-color: ${theme.section_one_background};
+  border-radius: 1rem;
+  margin: 0.5rem auto 2rem;
+  padding: 0.5rem 1rem;
+  font-weight: bold;
+  font-size: 1.2rem;
+  max-width: 75%;
+`;
 
 export function PriceList() {
   return (
-    <>
+    <PriceListWrapper>
       <h1>{dictionary.price_page.price_page_head}</h1>
-      <h2>{dictionary.price_page.price_page_subscription}</h2>
-      <p>{dictionary.price_page.price_page_subscription_price}</p>
-      <h2>{dictionary.price_page.price_page_one_offer}</h2>
-      <p>{dictionary.price_page.price_page_one_offer_price}</p>
-      <h2>{dictionary.price_page.price_page_vip_offer}</h2>
-      <p>{dictionary.price_page.price_page_vip_price}</p>
+      <StyledPriceHeader>
+        {dictionary.price_page.price_page_subscription}
+      </StyledPriceHeader>
+      <StyledPrice>
+        {dictionary.price_page.price_page_subscription_price}
+      </StyledPrice>
+      <StyledPriceHeader>
+        {dictionary.price_page.price_page_one_offer}
+      </StyledPriceHeader>
+      <StyledPrice>
+        {dictionary.price_page.price_page_one_offer_price}
+      </StyledPrice>
+      <StyledPriceHeader>
+        {dictionary.price_page.price_page_vip_offer}
+      </StyledPriceHeader>
+      <StyledPrice>{dictionary.price_page.price_page_vip_price}</StyledPrice>
       <h1>{dictionary.price_page.price_page_info_head}</h1>
       <h3>{dictionary.price_page.price_page_info_one}</h3>
       <h3>{dictionary.price_page.price_page_info_two}</h3>
       <h3>{dictionary.price_page.price_page_info_three}</h3>
       <h3>{dictionary.price_page.price_page_info_four}</h3>
-    </>
+    </PriceListWrapper>
   );
 }
